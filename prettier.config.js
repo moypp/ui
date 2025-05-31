95472d2ac5c27a5c5b5af7cda63749ebae372e10
ppyom <ouob77@kakao.com>
2025-05-31 15:40:56 +0900

    🔧 ESLint / Prettier 설정 파일 작성

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
+
+export default config;
